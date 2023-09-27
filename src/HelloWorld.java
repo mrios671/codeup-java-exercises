95a3414a1e55a2dbe2f1d3930d6d45de8893b7d2
Milianigia Rios <nanirios671@gmail.com>
2023-09-27 14:18:08 -0500

    got through the first four questions of today's exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,23 @@
 public class HelloWorld {
     public static void main(String[] arguments){
         System.out.println("Hello, World!");
+
+        // Question 1
+        int myFavoriteNumber = 11;
+        System.out.println(myFavoriteNumber);
+
+        // Question 2
+        String myString = "Practicing Java for Codeup exercises.";
+        System.out.println(myString);
+
+        //Question 3
+        //String myString = 'Practicing Java for Codeup exercises.';
+        //gave this error ---> java: unclosed character literal
+
+        //Question 4
+        //myString= 3.14159;
+        //gave this error ---> java: incompatible types: double cannot be converted to java.lang.String
+
+
     }
 }
