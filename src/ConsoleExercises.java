136ba43e96a081e4168adaa67a85bcece8174e43
Milianigia Rios <nanirios671@gmail.com>
2023-09-28 10:12:19 -0500

    finished with the exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -14,19 +14,32 @@ public class ConsoleExercises {
         //System.out.println("Wow! " + userAnswer + " is my favorite number too!");
 
         //Question 3
-        System.out.print("Enter your first word:");
-        String first = myScanner.nextLine();
+        //System.out.print("Enter your first word:");
+        //String first = myScanner.nextLine();
 
-        System.out.print("Enter your second word:");
-        String second = myScanner.nextLine();
+        //System.out.print("Enter your second word:");
+        //String second = myScanner.nextLine();
 
-        System.out.print("Enter your third word:");
-        String third = myScanner.nextLine();
+        //System.out.print("Enter your third word:");
+        //String third = myScanner.nextLine();
 
-        System.out.println("You entered the following words:");
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
+        //System.out.println("You entered the following words:");
+        //System.out.println(first);
+        //System.out.println(second);
+        //System.out.println(third);
+
+        //Question 4
+        System.out.println("Enter the length: ");
+        int length = Integer.parseInt(myScanner.nextLine());
+
+        System.out.println("Enter the width: ");
+        int width = Integer.parseInt(myScanner.nextLine());
+
+        System.out.println("The area of the rectangle is: ");
+        System.out.println(length * width);
+
+        System.out.println("The perimeter of the rectangle is : ");
+        System.out.println((2 * length)+ (2 * width));
 
     }
 }
