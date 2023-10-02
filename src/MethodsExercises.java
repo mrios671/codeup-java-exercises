e1c357ce594f6d9efc620f7b05fe09475132347c
Milianigia Rios <nanirios671@gmail.com>
2023-10-02 16:44:27 -0500

    finished with the first part of the exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,7 +1,20 @@
 public class MethodsExercises {
     public static void main(String[] args){
-
+        System.out.println(addition(2,7));
+        System.out.println(subtraction(10, 5));
+        System.out.println(multiplication(5, 5));
+        System.out.println(division(9, 3));
     }
-
-
+    public static Integer addition(int input1, int input2){
+        return input1 + input2;
+    }
+    public static Integer subtraction(int input1, int input2){
+        return input1 - input2;
+    }
+    public static Integer multiplication(int input1, int input2){
+        return input1 * input2;
+    }
+    public static Integer division(int input1, int input2){
+        return input1/input2;
+    }
 }
