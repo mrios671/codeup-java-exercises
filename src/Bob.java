f4da73d55e13cf5f6da41d163e28c308630fd84d
Milianigia Rios <nanirios671@gmail.com>
2023-10-02 15:06:57 -0500

    finsihed witht he exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,3 +1,40 @@
+import java.util.Scanner;
 public class Bob {
+    public static void main(String[] args) {
 
+        boolean talkMore = true;
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Talk to Bob...");
+
+        do{
+            String userInput = myScanner.nextLine();
+
+            String question = "Sure.";
+            String yell = "Whoa, chill out!";
+            String empty = "Fine, be that way!";
+            String anythingElse = "Whatever.";
+
+            if (userInput.endsWith("?")) {
+                System.out.println(question);
+            } else if (userInput.endsWith("!")) {
+                System.out.println(yell);
+            } else if (userInput == ("")) {
+                System.out.println(empty);
+            } else {
+                System.out.println(anythingElse);
+            }
+
+//            System.out.println("Do you want to ask Bob another question?");
+//            myScanner.nextLine();
+//            String userReply = myScanner.nextLine();
+//
+//            if(userReply.toLowerCase().equals("yes")){
+//                talkMore =true;
+//            }else{
+//                talkMore =false;
+//            }
+
+        } while(talkMore);
+
+    }
 }
