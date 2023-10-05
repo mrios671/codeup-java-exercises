5e4b16f40d70a804a0d030653fa7d49b78c6be7d
Milianigia Rios <nanirios671@gmail.com>
2023-10-04 20:13:43 -0500

    Finished with exercise

---
+++ src/util/Input.java
@@ -0,0 +1,58 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    public Input(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString(){
+        System.out.println("Enter a string: ");
+        String input = scanner.nextLine();
+        return input;
+    }
+
+    public boolean yesNo(){
+        System.out.println("Would you like to continue?");
+        String userInput = scanner.nextLine();
+        return userInput.equals("y") || userInput.equals("yes");
+    }
+
+    public int getInt(int min, int max){
+        System.out.println("Please enter a number between 1 and 10");
+        int userInput = scanner.nextInt();
+
+        if(userInput < min || userInput > max){
+            System.out.println("That number is invalid... please try again...");
+            return getInt(min, max);
+        }
+        return userInput;
+    }
+
+    public int getInt(){
+        System.out.println("Enter a Number: ");
+        int userInput = scanner.nextInt();
+        return userInput;
+    }
+
+    public double getDouble(double min, double max){
+        System.out.println("please enter a decimal number between 1 and 10");
+        double userInput = scanner.nextDouble();
+
+        if(userInput < min || userInput > max){
+            System.out.println("That number is invalid... please try again...");
+            return getDouble(min, max);
+        }
+        return userInput;
+    }
+
+    public double getDouble(){
+        System.out.println("Enter a decimal number: ");
+        double userInput = scanner.nextDouble();
+        return userInput;
+    }
+
+}
---
+++ src/util/InputTest.java
@@ -0,0 +1,29 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input stringInput = new Input();
+        String testString = stringInput.getString();
+        System.out.println(testString);
+
+        Input boolInput = new Input();
+        boolean boolString = boolInput.yesNo();
+        System.out.println(boolString);
+
+        Input intInput = new Input();
+        int test = intInput.getInt(1, 10);
+        System.out.println(test);
+
+        Input getIntInput = new Input();
+        int testTwo = getIntInput.getInt();
+        System.out.println(testTwo);
+
+        Input getDbl = new Input();
+        double dblTest = getDbl.getDouble(1.00, 10.00);
+        System.out.println(dblTest);
+
+        Input getSingleDouble = new Input();
+        double dblTestTwo = getSingleDouble.getDouble();
+        System.out.println(dblTestTwo);
+    }
+}
